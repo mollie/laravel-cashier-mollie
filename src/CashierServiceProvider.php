01a5f1cf8b6462b445726d8f4116e97fa0d3e9fc
Sander van Hooft <info@sandervanhooft.nl>
2021-11-17 11:29:01 +0100

    Prepare v2 release

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -18,7 +18,7 @@ class CashierServiceProvider extends ServiceProvider
 {
     use RegistersMollieInteractions;
 
-    const PACKAGE_VERSION = '0.2.7';
+    const PACKAGE_VERSION = '2.0.0';
 
     /**
      * Bootstrap the application services.
