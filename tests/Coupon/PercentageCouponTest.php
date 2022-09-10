e93563c4b41baa045e65e33cf5f54c7104a59b74
DirkvanBruxvoort <44770894+DirkvanBruxvoort@users.noreply.github.com>
2022-09-10 17:23:53 +0200

    Add couponCalculatesTheRightPriceBasedOnSubTotal test

--- tests/Coupon/PercentageCouponTest.php
+++ tests/Coupon/PercentageCouponTest.php
@@ -48,6 +48,42 @@ class PercentageCouponTest extends BaseTestCase
 
         $result = $preprocessor->handle($item->toCollection());
 
+        // discount = order item unit price / 100 * (100 + tax percentage) / 100 * percentage => 12150 / 100 * (100 + 21.5) / 100 * 20 = 2952
         $this->assertEquals(-2952, $result[1]->unit_price);
+    }
+
+    /** @test */
+    public function couponCalculatesTheRightPriceBasedOnSubTotal()
+    {
+        $couponHandler = new PercentageDiscountHandler;
+
+        $context = [
+            'description' => 'Percentage coupon',
+            'percentage' => 20,
+            'discount_on_subtotal' => true,
+        ];
+
+        $coupon = new Coupon(
+            'percentage-coupon',
+            $couponHandler,
+            $context
+        );
+
+        $this->withMockedCouponRepository($coupon, $couponHandler, $context);
+
+        /** @var Subscription $subscription */
+        $subscription = factory(Cashier::$subscriptionModel)->create();
+        $item = factory(Cashier::$orderItemModel)->make();
+        $subscription->orderItems()->save($item);
+
+        /** @var \Laravel\Cashier\Coupon\Coupon $coupon */
+        $coupon = app()->make(CouponRepository::class)->findOrFail('percentage-coupon');
+        $redeemedCoupon = $coupon->redeemFor($subscription);
+        $preprocessor = new CouponOrderItemPreprocessor();
+
+        $result = $preprocessor->handle($item->toCollection());
+
+        // discount = order item unit price / 100 * percentage => 12150 / 100 * 20 = 2430
+        $this->assertEquals(-2430, $result[1]->unit_price);
     }
 }
