83fb9300b64bf81f7758b70cbfea14891a5261be
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2022-10-27 11:49:43 +0300

    Update version number

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -17,7 +17,7 @@ class CashierServiceProvider extends ServiceProvider
 {
     use RegistersMollieInteractions;
 
-    const PACKAGE_VERSION = '1.16.2';
+    const PACKAGE_VERSION = '1.17.1';
 
     /**
      * Bootstrap the application services.
