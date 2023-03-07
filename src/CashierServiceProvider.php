bd0611a8791a2d5f95105be288204bb8fa8eb158
Krishan König <Naoray@users.noreply.github.com>
2023-03-07 09:16:07 +0100

    Update CashierServiceProvider.php
    Revert version update, since the related code update is in preparation of Laravel v10

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -19,7 +19,7 @@ class CashierServiceProvider extends ServiceProvider
 {
     use RegistersMollieInteractions;
 
-    const PACKAGE_VERSION = '2.6.2';
+    const PACKAGE_VERSION = '2.6.1';
 
     /**
      * Bootstrap the application services.
