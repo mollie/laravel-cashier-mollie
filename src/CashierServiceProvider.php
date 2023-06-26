7020478cdc19f170617ebc9a377b03b462125efe
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2023-06-26 12:39:44 +0200

    Update version number

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -19,7 +19,7 @@ class CashierServiceProvider extends ServiceProvider
 {
     use RegistersMollieInteractions;
 
-    const PACKAGE_VERSION = '2.7.0';
+    const PACKAGE_VERSION = '2.7.1';
 
     /**
      * Bootstrap the application services.
