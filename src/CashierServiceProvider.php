a8d95555f15861e32ac58497677233032f4fbf5e
Krishan König <Naoray@users.noreply.github.com>
2023-07-18 10:13:02 +0200

    Update CashierServiceProvider.php

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -19,7 +19,7 @@ class CashierServiceProvider extends ServiceProvider
 {
     use RegistersMollieInteractions;
 
-    const PACKAGE_VERSION = '2.8.1';
+    const PACKAGE_VERSION = '2.8.0';
 
     /**
      * Bootstrap the application services.
